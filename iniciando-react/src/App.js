07d18e42ec1df8d925d3360ba661f167e6b627ee
Marcelo Marques de Lima <72136257+marcelomarqueslima-projetodev@users.noreply.github.com>
2021-04-07 21:22:06 -0300

    Criando projeto cadastro e termino o projeto inicial

--- iniciando-react/src/App.js
+++ iniciando-react/src/App.js
@@ -1,15 +1,48 @@
 import React from 'react';
 
-function ComponentFuncional() {
-  return (
-    <h1>Hello World</h1>
-  )
-}
-
 class App extends React.Component{
+
+  state = {
+    nome : ' '
+  }
+
+  modificarNome = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = ["", "Fulano", "Deutrano"]
+
+    const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+
+    return(
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  componentDidMount(){
+    console.log('Executou o componentDidMount')
+  }
+
   render(){
+    console.log('Executou o render')
+
+    const MeuComboBox = () => this.criaComboBox()
     return (
-      <h1>Hello World - Class</h1>
+      /*<React.Fragment>
+        <h1>Hello World, {this.state.nome}</h1>
+        <h1>Testando</h1>
+        <!--{ this.criaComboBox() }-->
+      </React.Fragment>*/
+      <>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello World, {this.state.nome}</h1>
+        <MeuComboBox />
+      </>
     )
   }
 }
